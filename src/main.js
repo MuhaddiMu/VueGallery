900b8a5a2b1ac8bee999ab2d67e13bf1d7e4ee5f
MuhaddiMu <muhaddisshah@gmail.com>
2019-05-20 06:31:53 +0500

    Argon Dashboard Added by Creative Tim

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
+import ArgonDashboard from './plugins/argon-dashboard'
 
 Vue.config.productionTip = false
 
+Vue.use(ArgonDashboard)
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
