68e7c1bb77dba33f6eb7cda6e796b6f1f7fd0c00
MuhaddiMu <muhaddisshah@gmail.com>
2019-05-21 05:37:15 +0500

    Gallery View Updated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,25 @@
 <template>
     <body id="body" class="text-center">
-    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      
+    <div class="d-flex h-100 p-3 mx-auto flex-column">
       <header class="masthead mb-auto text-center">
         <div class="inner">
           <h3 class="">Image Gallery Vue.Js</h3>
         </div>
       </header>
 
-      <main role="main" class="inner cover">
-        <h1 class="cover-heading">Cover your page.</h1>
-        <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-        <p class="lead">
-          <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
-        </p>
-      </main>
+  <div class="container mt-4">
+    <div class="row">
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img1.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img2.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img3.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img4.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img5.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img6.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img7.jpg" /></a></div>
+      <div class="col-md-3 col-sm-4 col-6 Zoom"><a href="#"><img class="img-fluid" src="https://vuejsbook.com/img/vuejs/img8.jpg" /></a></div>
+    </div>
+  </div>
 
       <footer class="mastfoot mt-auto">
         <div class="inner">
@@ -39,4 +45,27 @@ export default {
 
 @import 'https://getbootstrap.com/docs/4.0/examples/cover/cover.css';
 
+img {
+    -webkit-box-shadow: 0px 1px 6px 1px rgba(0,0,0,0.2);
+    -moz-box-shadow: 0px 1px 6px 1px rgba(0,0,0,0.2);
+    box-shadow: 0px 1px 6px 1px rgba(0,0,0,0.2);
+    margin-bottom:20px;
+  }
+
+  img:hover {
+    filter: gray; /* IE6-9 */
+    -webkit-filter: grayscale(1); /* Google Chrome, Safari 6+ & Opera 15+ */
+  }
+
+  .Zoom img{
+  cursor: pointer;
+  transition: transform 2s, filter 1.2s ease-in-out;
+  transform-origin: center center;
+  filter: brightness(50%);
+  }
+
+  .Zoom img:hover {
+    filter: brightness(100%);
+    transform: scale(1.05);
+  }
 </style>
